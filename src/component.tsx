c012356b2ac1873cb1649eee77561c9c66466ff2
simon <simon.roberge@vimaec.com>
2022-06-13 15:15:02 -0400

    ui menu

--- src/component.tsx
+++ src/component.tsx
@@ -2,25 +2,41 @@
 // eslint-disable-next-line no-use-before-define
 import React, { useEffect, useState } from 'react'
 import logo from './assets/logo.png'
+import iconOrbit from './assets/icon_orbit.png'
+import iconFirstPerson from './assets/icon_firstperson.png'
+import iconOrtho from './assets/icon_ortho.png'
+import iconPerspective from './assets/icon_perspective.jpg'
+import iconFocus from './assets/icon_focus.png'
+import iconHome from './assets/icon_home.png'
+import iconHide from './assets/icon_hide.png'
+import iconShowAll from './assets/icon_showall.jpg'
+
 import * as VIM from 'vim-webgl-viewer/'
 import './style.css'
-
-const canvasId = 'vim-canvas'
 
 type Progress = 'processing'| number | string
 type Table = [string, string][]
 const round2 = (n) => Math.round((n + Number.EPSILON) * 100) / 100
 
-export function createRoot(){
-  const r = document.createElement('div')
-  r.className = 'vim'
-  r.style.height = '100%'
-  document.body.append(r)
-  return r
+export function createContainer(viewer: VIM.Viewer){
+  const root = document.createElement('div')
+  root.className = 'vim-component'
+  root.style.height = '100%'
+  document.body.append(root)
+
+  root.append(viewer.viewport.canvas)
+
+  const ui = document.createElement('div')
+  ui.className = 'vim-ui'
+  ui.style.height = '100%'
+  root.append(ui)
+
+  return ui
 }
 
 export function VimComponent (props: {
-  onViewerReady: (vim: VIM.Viewer) => void
+  viewer: VIM.Viewer,
+  onMount: () => void,
   logo?: boolean,
   inspector?: boolean,
   menu?: boolean,
@@ -30,66 +46,93 @@ export function VimComponent (props: {
   const useInspector = props.inspector === undefined ? true: props.inspector
   const useMenu = props.menu === undefined ? true: props.menu
   const useLoading = props.loading === undefined ? true: props.loading
-
-  const [progress, setProgress] = useState<Progress>()
-  const [table, setTable] = useState<Table>()
+  
   useEffect(() => {
-    const viewer = new VIM.Viewer({ 
-      canvas: {id : canvasId},
-      groundPlane: {
-        show: true,
-        texture:
-          'https://vimdevelopment01storage.blob.core.windows.net/textures/vim-floor-soft.png',
-        opacity: 1,
-        size: 5
-      }
-    })
-  
-    // Patch on click
-    const previous = viewer.onMouseClick.bind(viewer)
-    viewer.onMouseClick = (hit) => {
-      previous(hit)
-      createTable(hit.object).then(t => setTable(t))
-    }
-
-      // Patch load
-    const prev = viewer.loadVim.bind(viewer)
-    viewer.loadVim = function (source: string| ArrayBuffer, options: VIM.VimOptions.Root, _ : (logger: VIM.IProgressLogs) => void) : Promise<VIM.Vim>{
-      return prev(source, options, (p) => {
-        setProgress(p.loaded)
-      }).then(_ =>setProgress(undefined))
-    }
-    props.onViewerReady(viewer)
+    console.log('props.onMount()')
+    props.onMount()
   }, [])
-  
+
   return (
     <>
-      <canvas id={canvasId}> </canvas>
       {useLogo ? <Logo /> : null}
-      {useLoading ? <LoadingBox progress={progress} /> : null}
-      {useInspector ? <Inspector data={table}/> : null}
-      {useMenu ? <Menu/> : null}
+      {useLoading ? <LoadingBox viewer={props.viewer}/> : null}
+      {useInspector ? <Inspector viewer={props.viewer}/> : null}
+      {useMenu ? <Menu viewer={props.viewer}/> : null}
     </>
   )
 }
 
-function Menu(){
+function Menu(props: {viewer: VIM.Viewer}){
+  console.log('render menu')
+
+  const viewer = props.viewer
+  const [orbit, setObit] = linkState(viewer.camera, 'orbitMode')
+  const [ortho, setOrtho] = linkState(viewer.camera, 'orthographic')
+  const [selection, setSelection] = useState<VIM.Object>(viewer.selection.object)
+  
+  
+  useEffect(() => {
+  // Patch Selection Select
+    const prevSelect = viewer.selection.select.bind(viewer.selection)
+    viewer.selection.select = (obj) => {
+      prevSelect(obj)
+      setSelection(obj)
+    }
+
+    // Patch Selection Clear
+    const prevClear = viewer.selection.clear.bind(viewer.selection)
+    viewer.selection.clear = () => {
+      prevClear()
+      setSelection(undefined)
+    }
+  },[])
+
+  const btnOrbit = <button className="iconButton" type="button"> <img src={orbit ? iconFirstPerson : iconOrbit} onClick={() => setObit(!orbit)} /></button>
+  const btnOrtho = <button className="iconButton" type="button"> <img src={ortho ? iconPerspective : iconOrtho} onClick={() => setOrtho(!ortho)} /></button>
+
+  const onFocusButton = function () {
+    if(!selection) return
+    viewer.camera.frame(selection, true, viewer.camera.defaultLerpDuration)
+  }
+
+  const btnFocus = <button  className="iconButton" type="button" disabled={!selection}><img src={iconFocus} onClick={onFocusButton} /></button>
+  iconHome
+  const onHomeButton = function(){
+    viewer.camera.frame('all', true, viewer.camera.defaultLerpDuration)
+  }
+  const btnHome = <button className="iconButton"  type="button"><img src={iconHome} onClick={onHomeButton} /></button>
+
+  const onHideButton = function(){
+    if(!selection) return
+    selection.visible = false
+    viewer.selection.clear()
+  }
+  const btnHide = <button className="iconButton" type="button" disabled={!selection}><img src={iconHide} onClick={onHideButton} /></button>
+
+  const onShowAllButton = function(){
+    for(const o of viewer.vims[0].getAllObjects()){
+      o.visible = true
+    }
+  }
+
+  const btnShowAll = <button  className="iconButton" type="button"><img src={iconShowAll} onClick={onShowAllButton} /></button>
+
   return <div className="vim-menu">
     <table>
       <tbody>
-      <tr><td><button type="button">Camera</button></td></tr>
-      <tr><td><button type="button">Ortho</button></td></tr>
-      <tr><td><button type="button">Focus</button></td></tr>
-      <tr><td><button type="button">Home</button></td></tr>
-      <tr><td><button type="button">Fullscreen</button></td></tr>
-      <tr><td><button type="button">Hide</button></td></tr>
-      <tr><td><button type="button">Show All</button></td></tr>
+      <tr><td>{btnOrbit}</td></tr>
+      <tr><td>{btnOrtho}</td></tr>
+      <tr><td>{btnFocus}</td></tr>
+      <tr><td>{btnHome}</td></tr>
+      <tr><td>{btnHide}</td></tr>
+      <tr><td>{btnShowAll}</td></tr>
       </tbody>
     </table>
   </div>
 }
 
 function Logo () {
+  console.log('Logo')
   return (
     <div className="vim-logo">
       <a href="https://vimaec.com">
@@ -99,11 +142,24 @@ function Logo () {
   )
 }
 
-function LoadingBox (prop: { progress: Progress }) {
+function LoadingBox (props: { viewer: VIM.Viewer }) {
+  console.log('LoadingBox')
+  const [progress, setProgress] = useState<Progress>()
+
+  // Patch load
+  useEffect(() => {
+    const prevLoad = props.viewer.loadVim.bind(props.viewer)
+    props.viewer.loadVim = function (source: string| ArrayBuffer, options: VIM.VimOptions.Root, _ : (logger: VIM.IProgressLogs) => void) : Promise<VIM.Vim>{
+      return prevLoad(source, options, (p) => {
+        setProgress(p.loaded)
+      }).then(_ =>setProgress(undefined))
+    }
+  },[])
+
   const msg = 
-  prop.progress ==='processing' ? 'Processing'
-  : typeof(prop.progress) === 'number' ? `Downloading: ${Math.round(prop.progress / 1000000)} MB`
-  : typeof(prop.progress) === 'string' ? `Error: ${prop.progress}`
+  progress ==='processing' ? 'Processing'
+  : typeof(progress) === 'number' ? `Downloading: ${Math.round(progress / 1000000)} MB`
+  : typeof(progress) === 'string' ? `Error: ${progress}`
   : undefined
 
   if (!msg) return null
@@ -114,19 +170,47 @@ function LoadingBox (prop: { progress: Progress }) {
   )
 }
 
-function Inspector(prop: { data: Table })
+function Inspector(props: { viewer: VIM.Viewer })
 {
-  if(!prop.data) return null
+  const viewer = props.viewer
+  useEffect(() => {
+  // Patch Selection Select
+    const prevSelect = viewer.selection.select.bind(viewer.selection)
+    viewer.selection.select = (obj) => {
+      prevSelect(obj)
+      createTable(obj).then(t => setTable(t))
+    }
+
+    // Patch Selection Clear
+    const prevClear = viewer.selection.clear.bind(viewer.selection)
+    viewer.selection.clear = () => {
+      prevClear()
+      setTable(undefined)
+    }
+  },[])
+
+  const [table, setTable] = useState<Table>()
+
+  // Patch on click
+  useEffect(() => {
+    const prevClick = props.viewer.onMouseClick.bind(props.viewer)
+    props.viewer.onMouseClick = (hit) => {
+      prevClick(hit)
+      createTable(hit.object).then(t => setTable(t))
+    }
+  })
+
+  if(!table) return null
 
   const set = new Set(["Type", "Name", "FamilyName", "Id"])
-  const mains = prop.data.filter(pair => set.has(pair[0])).map((pair, index) => {
+  const mains = table.filter(pair => set.has(pair[0])).map((pair, index) => {
     return <tr key={'main-tr' + index} >
       <th key={'main-th' + index}>{pair[0]}</th>
       <td key={'main-td' + index}>{pair[1]}</td>
     </tr>
   })
   
-  const details = prop.data.filter(pair => !set.has(pair[0])).map((pair, index) => {
+  const details = table.filter(pair => !set.has(pair[0])).map((pair, index) => {
     return <tr key={'details-tr' + index} >
       <th key={'details-th' + index}>{pair[0]}</th>
       <td key={'details-td' + index}>{pair[1]}</td>
@@ -159,7 +243,6 @@ function Inspector(prop: { data: Table })
   )
 }
 
-
 async function createTable(object: VIM.Object): Promise<[string, string][]>{
   
   if(!object){
@@ -178,4 +261,30 @@ async function createTable(object: VIM.Object): Promise<[string, string][]>{
     table.push([key, value])
   }
   return table
+}
+
+function linkState(o: Object, key: string){
+  const [_, setState] = useState<boolean>(o[key])
+  const {get, set} = patchProperty(o, key, setState.bind(o))
+  return [get(), set]
+}
+
+function patchProperty(o: Object, key: string, onSet: (v:any) => void){
+  
+  const proto = Object.getPrototypeOf(o)
+  const prev = Object.getOwnPropertyDescriptor(proto, key)
+
+  const get = function(){ return prev.get.apply(o) }
+  const set = function(value:any){{
+    console.log('set' + key)
+    prev.set.apply(o, [value])
+    onSet(value)
+  }}
+  Object.defineProperty(o, key,
+  {
+    configurable : true,
+    get: get,
+    set:set,
+  })
+  return {get , set}
 }
