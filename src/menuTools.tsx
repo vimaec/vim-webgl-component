13224e88b8bcc3a7bed9d6585b9a026e5d5a987b
simon <simon.roberge@vimaec.com>
2022-07-22 14:20:05 -0400

    changed the measure flow to loop until out of measure mode

--- src/menuTools.tsx
+++ src/menuTools.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react'
 import * as VIM from 'vim-webgl-viewer/'
 import * as Icons from './icons'
@@ -10,6 +10,10 @@ export function MenuTools(props: {viewer: VIM.Viewer, moreMenuVisible:boolean, s
   const [measuring, setMeasuring] = useState(false)
   const [measurement, setMeasurement] = useState<VIM.THREE.Vector3>()
   
+  // Need a ref to get the up to date value in callback.
+  const measuringRef = useRef<boolean>()
+  measuringRef.current = measuring
+  
   const onHomeButton = function(){
     viewer.camera.frame('all', true, viewer.camera.defaultLerpDuration)
   }
@@ -19,19 +23,38 @@ export function MenuTools(props: {viewer: VIM.Viewer, moreMenuVisible:boolean, s
     viewer.gizmoSection.visible = !section
     setSection(!section)
   }
+  
+  const loopMeasure = () => {
+    
+    const onMouseMove = () =>{
+      console.log(props.viewer.gizmoMeasure.measurement)
+      setMeasurement(props.viewer.gizmoMeasure.measurement)
+    } 
+
+    viewer.viewport.canvas.addEventListener('mousemove', onMouseMove)
+    viewer.gizmoMeasure.measure()
+    .then(() => {
+      setMeasurement(viewer.gizmoMeasure.measurement)
+    })
+    .catch(() => {
+      setMeasurement(undefined)
+    })
+    .finally(() => {
+      viewer.viewport.canvas.removeEventListener('mousemove', onMouseMove)
+      if(measuringRef.current) {
+        loopMeasure()
+      }
+    })
+  }
 
   const onMeasureBtn = () => {
     if(measuring){
-      viewer.gizmoMeasure.clear()
+      viewer.gizmoMeasure.abort()
       setMeasuring(false)
     }
     else{
-      setMeasurement(undefined)
       setMeasuring(true)
-      viewer.gizmoMeasure.measure()
-      .then(() => setMeasurement(viewer.gizmoMeasure.measurement))
-      .catch(() => setMeasurement(undefined))
-      .finally(() => setMeasuring(false))
+      loopMeasure()
     }
   }
 
@@ -41,19 +64,19 @@ export function MenuTools(props: {viewer: VIM.Viewer, moreMenuVisible:boolean, s
 
   const btnHome = <button onClick={onHomeButton} className={`rounded-full text-white h-12 w-12 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40 disabled:opacity-50`} type="button"><Icons.Home height="32" width="32" fill="currentColor" /></button>
   const btnSection = <button onClick={onSectionButton} className={`rounded-full text-white h-12 w-12 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40 ${section ? 'bg-primary-royal hover:bg-primary-royal' : ''}`} type="button"><Icons.Box height="32" width="32" fill="currentColor" /></button>
-  const btnMeasure = <button onClick={onMeasureBtn} className={`rounded-full text-white h-12 w-12 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40 ${measurement || measuring ? 'bg-primary-royal hover:bg-primary-royal' : ''}`} type="button"><Icons.Measure height="32" width="32" fill="currentColor" /></button>
+  const btnMeasure = <button onClick={onMeasureBtn} className={`rounded-full text-white h-12 w-12 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40 ${measuring ? 'bg-primary-royal hover:bg-primary-royal' : ''}`} type="button"><Icons.Measure height="32" width="32" fill="currentColor" /></button>
   const btnMore = <button onClick={onMoreBtn} className={`rounded-full text-white h-12 w-12 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40 ${props.moreMenuVisible ? 'bg-primary-royal hover:bg-primary-royal' : ''}`} type="button"><Icons.More height="32" width="32" fill="currentColor" /></button>
 
 
   const txtMeasure = <ul className='flex text-white mr-auto'>
-    <li className='mr-3'><strong>Dist</strong>: {measurement?.length().toFixed(2)}</li>
-    <li className='mr-3'><strong>X</strong>: {measurement?.x.toFixed(2)}</li>
-    <li className='mr-3'><strong>Y</strong>: {measurement?.y.toFixed(2)}</li>
-    <li className='mr-3'><strong>Z</strong>: {measurement?.z.toFixed(2)}</li>
+    <li key="dist" className='mr-3'><strong>Dist</strong>: {measurement?.length().toFixed(2)}</li>
+    <li key="x" className='mr-3'><strong>X</strong>: {measurement?.x.toFixed(2)}</li>
+    <li key="y" className='mr-3'><strong>Y</strong>: {measurement?.y.toFixed(2)}</li>
+    <li key="z" className='mr-3'><strong>Z</strong>: {measurement?.z.toFixed(2)}</li>
   </ul>
   
-  return <div className={`vim-menu flex items-center justify-end w-full fixed px-2 bottom-0 py-2 ${measurement ? 'bg-hover-t40' : ''}`}>
-    {(measurement) ? txtMeasure : ''}
+  return <div className={`vim-menu flex items-center justify-end w-full fixed px-2 bottom-0 py-2 ${measuring ? 'bg-hover-t40' : ''}`}>
+    {measuring ? txtMeasure : ''}
     <div className='mx-1'>{btnSection}</div>
     <div className='mx-1'>{btnMeasure}</div>
     {/* <div className='mx-1'>{btnHome}</div> */}
