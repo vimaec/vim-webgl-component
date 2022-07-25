bb4c8b6c42b0b76f8dc7b4eabb524c763260d10a
Artem Kom <artem.kom@gmail.com>
2022-07-25 15:14:31 +0300

    center canvas in sidebar is open

--- src/menuTop.tsx
+++ src/menuTop.tsx
@@ -23,7 +23,7 @@ export function MenuTop(props: {
 
   const btnOrbit = <button onClick={() => props.setOrbit(!props.orbit)} className={"rounded-full text-white h-8 w-8 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40"} type="button">{props.orbit ? <Icons.Orbit height="20" width="20" fill="currentColor" /> : <Icons.FirstPerson height="20" width="20" fill="currentColor" />}</button>
   const btnOrtho = <button onClick={() => props.setOrtho(!props.ortho)} className={"rounded-full text-white h-8 w-8 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40"} type="button">{props.ortho ? <Icons.Orthographic height="20" width="20" fill="currentColor" /> : <Icons.Perspective height="20" width="20" fill="currentColor" />}</button>
-  const btnCamera = <button className={"rounded-full text-white text-sm h-8 w-8 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40 opacity-75"} type="button"><Icons.Camera height="20" width="20" fill="currentColor" />{speed}</button>
+  const btnCamera = <div className={"rounded-full text-white text-sm h-8 w-8 flex items-center justify-center transition-all opacity-75"} type="button"><Icons.Camera height="20" width="20" fill="currentColor" />{speed}</div>
   
   return <div className='flex flex-col mx-2 my-0 fixed right-0 top-2 w-auto'>
     <div className='border border-hover-t40 h-28 w-full rounded-t-md'> </div>
