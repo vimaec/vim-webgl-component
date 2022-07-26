74877e13f41eec326b7aa13fa99ae99da06ee498
simon <simon.roberge@vimaec.com>
2022-07-26 15:51:48 -0400

    fixed axes gizmo not receiving clicks

--- src/menuTop.tsx
+++ src/menuTop.tsx
@@ -25,9 +25,9 @@ export function MenuTop(props: {
   const btnOrtho = <button onClick={() => props.setOrtho(!props.ortho)} className={"rounded-full text-white h-8 w-8 flex items-center justify-center transition-all hover:scale-110 hover:bg-hover-t40"} type="button">{props.ortho ? <Icons.Orthographic height="20" width="20" fill="currentColor" /> : <Icons.Perspective height="20" width="20" fill="currentColor" />}</button>
   const btnCamera = <div className={"rounded-full text-white text-sm h-8 w-8 flex items-center justify-center transition-all opacity-75"} type="button"><Icons.Camera height="20" width="20" fill="currentColor" />{speed}</div>
   
-  return <div className='flex flex-col mx-2 my-0 fixed right-0 top-2 w-auto'>
-    <div className='border border-hover-t40 h-28 w-full rounded-t-md'> </div>
-    <div className="vim-menu flex bg-hover-t40 p-1 rounded-b-md">
+  return <div className='vim-top flex flex-col mx-2 my-0 fixed right-0 top-2 w-auto pointer-events-none'>
+    <div className='border border-hover-t40 h-28 w-full rounded-t-md pointer-events-none' > </div>
+    <div className="flex bg-hover-t40 p-1 rounded-b-md pointer-events-auto">
       <div className='mx-1'>{btnOrbit}</div>
       <div className='mx-1'>{btnOrtho}</div>
       <div className='mx-1'>{btnCamera}</div>
