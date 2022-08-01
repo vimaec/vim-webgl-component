c3e09907aad0798001e4b6a5a49259a5a1257b62
simon <simon.roberge@vimaec.com>
2022-08-01 12:26:13 -0400

    fixed top menu padding and gizmo size.

--- src/menuTop.tsx
+++ src/menuTop.tsx
@@ -26,7 +26,7 @@ export function MenuTop(props: {
   const btnOrtho = <button onClick={() => props.setOrtho(!props.ortho)} className={"rounded-full text-white h-8 w-8 flex items-center justify-center transition-all opacity-80 hover:opacity-100"} type="button">{props.ortho ? <Icons.Orthographic height="20" width="20" fill="currentColor" /> : <Icons.Perspective height="20" width="20" fill="currentColor" />}</button>
   const btnCamera = <div className={"rounded-full text-white text-sm h-8 w-8 flex items-center justify-center transition-all opacity-75"}><Icons.Camera height="20" width="20" fill="currentColor" />{speed}</div>
   
-  return <div className='vim-top flex flex-col mx-2 my-0 fixed right-0 top-2 w-auto pointer-events-none'>
+  return <div className='vim-top flex flex-col fixed right-6 top-6 w-auto pointer-events-none'>
     <div className='border border-hover-t40 h-28 w-full rounded-t-md pointer-events-none' > </div>
     <div className="flex bg-hover-t40 p-1 rounded-b-md pointer-events-auto">
       <div className='mx-1'>{btnOrbit}</div>
