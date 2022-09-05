0450f216f8a765e6f759c5d5a13c1e1340e44709
Art <artem.kom@gmail.com>
2022-09-05 15:55:42 +0300

    bottom menu and gizmo styling

--- src/menuHelp.tsx
+++ src/menuHelp.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { useState } from 'react'
-import helpImage from './assets/help_controls.png'
+import helpImage from './assets/quick-controls.svg'
 import * as Icons from './icons'
 
 const urlSupport = 'https://support.vimaec.com'
@@ -23,10 +23,9 @@ return <>
   <div className="menu-help-overlay absolute inset-0 bg-white/50 px-[10%] py-[30%]" onClick={onCloseBtn}>
     <div className="menu-help-safezone py-5" onClick={(e) =>{e.stopPropagation()}}>
       <button className="menu-help-close w-4 h-4" onClick={onCloseBtn}>{Icons.close({height:"24px",width:"24px", fill:"currentColor" })}</button>
-      <img className="menu-help-controls" src={helpImage}></img>
-      <span className="h-2 block"/>
-      <button className="menu-help-fullcontrols w-40 h-6 bg-white rounded-xl" onClick={onControlsBtn}>Full Control List</button>
-      <button className="menu-help-helpcenter w-40 h-6 bg-white rounded-xl" onClick={onHelpBtn}>Help Center</button>
+      <img className="menu-help-controls mb-8" src={helpImage}></img>
+      <button className="text-white text-xs font-bold uppercase bg-primary hover:bg-primary-royal rounded-full py-2 px-8 mr-4" onClick={onControlsBtn}>Full Control List</button>
+      <button className="text-white text-xs font-bold uppercase bg-primary hover:bg-primary-royal rounded-full py-2 px-8" onClick={onHelpBtn}>Help Center</button>
       </div>
   </div>
 </>
