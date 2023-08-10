5eb22146f4c7e839016dcf46a4b2e14590e667db
simon <simon.roberge@vimaec.com>
2023-08-10 14:40:10 -0400

    implemented more granular ui control

--- src/settings/menuSettings.tsx
+++ src/settings/menuSettings.tsx
@@ -21,7 +21,7 @@ export function MenuSettings (props: {
   if (!props.visible) return null
   const toggleElement = (label: string, state: boolean, action: () => void) => {
     return (
-      <label className="vc-m-1 vc-inline-flex vc-select-none vc-items-center vc-py-1 vc-text-gray-warm">
+      <label className="vc-m-1 vc-block vc-select-none vc-items-center vc-py-1 vc-text-gray-warm">
         <input
           type="checkbox"
           checked={state}
@@ -49,136 +49,138 @@ export function MenuSettings (props: {
   }
 
   return (
-    <div className="vc vc-m-1 vc-overflow-auto">
+    <>
       <h2 className="vc-mb-6 vc-text-xs vc-font-bold vc-uppercase">
         Display Settings
       </h2>
-      <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Viewer</h3>
-      {settingsToggle(
-        'Ghost Hidden Objects',
-        (settings) => settings.viewer.isolationMaterial,
-        (settings, value) => (settings.viewer.isolationMaterial = value)
-      )}
-      {settingsToggle(
-        'Show Ground Plane',
-        (settings) => settings.viewer.groundPlane,
-        (settings, value) => (settings.viewer.groundPlane = value)
-      )}
-      <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Panels</h3>
-      {settingsToggle(
-        'Show Logo',
-        (settings) => settings.ui.logo,
-        (settings, value) => (settings.ui.logo = value)
-      )}
-      {settingsToggle(
-        'Show Bim Tree',
-        (settings) => settings.ui.bimTreePanel,
-        (settings, value) => (settings.ui.bimTreePanel = value)
-      )}
-      {settingsToggle(
-        'Show Bim Info',
-        (settings) => settings.ui.bimInfoPanel,
-        (settings, value) => (settings.ui.bimInfoPanel = value)
-      )}
-      {settingsToggle(
-        'Show Axes',
-        (settings) => settings.ui.axesPanel,
-        (settings, value) => (settings.ui.axesPanel = value)
-      )}
-      {settingsToggle(
-        'Show Performance',
-        (settings) => settings.ui.performance,
-        (settings, value) => (settings.ui.performance = value)
-      )}
-      {settingsToggle(
-        'Show Logs',
-        (settings) => settings.ui.logPanel,
-        (settings, value) => (settings.ui.logPanel = value)
-      )}
-      <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Axes</h3>
-      {settingsToggle(
-        'Show Orthographic Button',
-        (settings) => settings.ui.orthographic,
-        (settings, value) => (settings.ui.orthographic = value)
-      )}
-      {settingsToggle(
-        'Show Reset Camera Button',
-        (settings) => settings.ui.resetCamera,
-        (settings, value) => (settings.ui.resetCamera = value)
-      )}
-      {settingsToggle(
-        'Show Toggle Ghost Button',
-        (settings) => settings.ui.enableGhost,
-        (settings, value) => (settings.ui.enableGhost = value)
-      )}
-      <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Cursors</h3>
-      {settingsToggle(
-        'Show Orbit Button',
-        (settings) => settings.ui.orbit,
-        (settings, value) => (settings.ui.orbit = value)
-      )}
-      {settingsToggle(
-        'Show Look Around Button',
-        (settings) => settings.ui.lookAround,
-        (settings, value) => (settings.ui.lookAround = value)
-      )}
-      {settingsToggle(
-        'Show Pan Button',
-        (settings) => settings.ui.pan,
-        (settings, value) => (settings.ui.pan = value)
-      )}
-      {settingsToggle(
-        'Show Zoom Button',
-        (settings) => settings.ui.zoom,
-        (settings, value) => (settings.ui.zoom = value)
-      )}
-      {settingsToggle(
-        'Show Zoom Window Button',
-        (settings) => settings.ui.zoomWindow,
-        (settings, value) => (settings.ui.zoomWindow = value)
-      )}
-      {settingsToggle(
-        'Show Zoom To Fit Button',
-        (settings) => settings.ui.zoomToFit,
-        (settings, value) => (settings.ui.zoomToFit = value)
-      )}
-      <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Tool</h3>
-      {settingsToggle(
-        'Show Sectioning Mode Button ',
-        (settings) => settings.ui.sectioningMode,
-        (settings, value) => (settings.ui.sectioningMode = value)
-      )}
-      {settingsToggle(
-        'Show Measuring Mode Button',
-        (settings) => settings.ui.measuringMode,
-        (settings, value) => (settings.ui.measuringMode = value)
-      )}
-      {settingsToggle(
-        'Show Toggle Isolation Button',
-        (settings) => settings.ui.toggleIsolation,
-        (settings, value) => (settings.ui.toggleIsolation = value)
-      )}
-      <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Settings</h3>
-      {settingsToggle(
-        'Show Project Inspector Button',
-        (settings) => settings.ui.projectInspector,
-        (settings, value) => (settings.ui.projectInspector = value)
-      )}
-      {settingsToggle(
-        'Show Settings Button',
-        (settings) => settings.ui.settings,
-        (settings, value) => (settings.ui.settings = value)
-      )}
-      {settingsToggle(
-        'Show Help Button',
-        (settings) => settings.ui.help,
-        (settings, value) => (settings.ui.help = value)
-      )}
-      {settingsToggle(
-        'Show Maximise Button',
-        (settings) => settings.ui.maximise,
-        (settings, value) => (settings.ui.maximise = value)
-      )}
-    </div>
+      <div className="vim-settings vc-m-1 vc-max-h-full vc-overflow-y-auto">
+        <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Viewer</h3>
+        {settingsToggle(
+          'Ghost Hidden Objects',
+          (settings) => settings.viewer.isolationMaterial,
+          (settings, value) => (settings.viewer.isolationMaterial = value)
+        )}
+        {settingsToggle(
+          'Show Ground Plane',
+          (settings) => settings.viewer.groundPlane,
+          (settings, value) => (settings.viewer.groundPlane = value)
+        )}
+        <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Panels</h3>
+        {settingsToggle(
+          'Show Logo',
+          (settings) => settings.ui.logo,
+          (settings, value) => (settings.ui.logo = value)
+        )}
+        {settingsToggle(
+          'Show Bim Tree',
+          (settings) => settings.ui.bimTreePanel,
+          (settings, value) => (settings.ui.bimTreePanel = value)
+        )}
+        {settingsToggle(
+          'Show Bim Info',
+          (settings) => settings.ui.bimInfoPanel,
+          (settings, value) => (settings.ui.bimInfoPanel = value)
+        )}
+        {settingsToggle(
+          'Show Axes',
+          (settings) => settings.ui.axesPanel,
+          (settings, value) => (settings.ui.axesPanel = value)
+        )}
+        {settingsToggle(
+          'Show Performance',
+          (settings) => settings.ui.performance,
+          (settings, value) => (settings.ui.performance = value)
+        )}
+        {settingsToggle(
+          'Show Logs',
+          (settings) => settings.ui.logPanel,
+          (settings, value) => (settings.ui.logPanel = value)
+        )}
+        <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Axes</h3>
+        {settingsToggle(
+          'Show Orthographic Button',
+          (settings) => settings.ui.orthographic,
+          (settings, value) => (settings.ui.orthographic = value)
+        )}
+        {settingsToggle(
+          'Show Reset Camera Button',
+          (settings) => settings.ui.resetCamera,
+          (settings, value) => (settings.ui.resetCamera = value)
+        )}
+        {settingsToggle(
+          'Show Toggle Ghost Button',
+          (settings) => settings.ui.enableGhost,
+          (settings, value) => (settings.ui.enableGhost = value)
+        )}
+        <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Cursors</h3>
+        {settingsToggle(
+          'Show Orbit Button',
+          (settings) => settings.ui.orbit,
+          (settings, value) => (settings.ui.orbit = value)
+        )}
+        {settingsToggle(
+          'Show Look Around Button',
+          (settings) => settings.ui.lookAround,
+          (settings, value) => (settings.ui.lookAround = value)
+        )}
+        {settingsToggle(
+          'Show Pan Button',
+          (settings) => settings.ui.pan,
+          (settings, value) => (settings.ui.pan = value)
+        )}
+        {settingsToggle(
+          'Show Zoom Button',
+          (settings) => settings.ui.zoom,
+          (settings, value) => (settings.ui.zoom = value)
+        )}
+        {settingsToggle(
+          'Show Zoom Window Button',
+          (settings) => settings.ui.zoomWindow,
+          (settings, value) => (settings.ui.zoomWindow = value)
+        )}
+        {settingsToggle(
+          'Show Zoom To Fit Button',
+          (settings) => settings.ui.zoomToFit,
+          (settings, value) => (settings.ui.zoomToFit = value)
+        )}
+        <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Tool</h3>
+        {settingsToggle(
+          'Show Sectioning Mode Button ',
+          (settings) => settings.ui.sectioningMode,
+          (settings, value) => (settings.ui.sectioningMode = value)
+        )}
+        {settingsToggle(
+          'Show Measuring Mode Button',
+          (settings) => settings.ui.measuringMode,
+          (settings, value) => (settings.ui.measuringMode = value)
+        )}
+        {settingsToggle(
+          'Show Toggle Isolation Button',
+          (settings) => settings.ui.toggleIsolation,
+          (settings, value) => (settings.ui.toggleIsolation = value)
+        )}
+        <h3 className="vc-mt-2 vc-text-xs vc-font-bold">Settings</h3>
+        {settingsToggle(
+          'Show Project Inspector Button',
+          (settings) => settings.ui.projectInspector,
+          (settings, value) => (settings.ui.projectInspector = value)
+        )}
+        {settingsToggle(
+          'Show Settings Button',
+          (settings) => settings.ui.settings,
+          (settings, value) => (settings.ui.settings = value)
+        )}
+        {settingsToggle(
+          'Show Help Button',
+          (settings) => settings.ui.help,
+          (settings, value) => (settings.ui.help = value)
+        )}
+        {settingsToggle(
+          'Show Maximise Button',
+          (settings) => settings.ui.maximise,
+          (settings, value) => (settings.ui.maximise = value)
+        )}
+      </div>
+    </>
   )
 }
