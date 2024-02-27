9e8aeef45153a49455ca05dc9a028f4155e6d7e0
simon <simon.roberge@vimaec.com>
2024-02-27 16:56:47 -0500

    added loading gizmo when applicable.

--- src/main.tsx
+++ src/main.tsx
@@ -19,8 +19,10 @@ createVimComponent(loadVim, undefined, getLocalSettings())
 async function loadVim (cmp: VimComponentRef) {
   const time = Date.now()
   globalThis.component = cmp
+  cmp.viewer.gizmos.loading.visible = true
   const vim = await cmp.loader.open(
-    'https://vim02.azureedge.net/samples/residence.v1.2.75.vim',
+    url,
+    //"https://vimdevelopment01storage.blob.core.windows.net/samples/residence.v1.2.75.vimx",
     {
       progressive: true,
       rotation: new THREE.Vector3(270, 0, 0)
@@ -30,7 +32,9 @@ async function loadVim (cmp: VimComponentRef) {
   cmp.viewer.add(vim)
   vim.loadAll()
   cmp.viewer.camera.do().frame(vim)
+  
   console.log(`Loading completed in ${((Date.now() - time)/1000).toFixed(2)} seconds`)
+  cmp.viewer.gizmos.loading.visible = false
 }
 
 globalThis.VIM = VIM
