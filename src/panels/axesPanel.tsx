15e43b50b52a514981b6cdcdf67014a3aeff2ef8
vim-sroberge <simon.roberge@vimaec.com>
2024-05-27 10:50:29 -0400

    responsive axes panel

--- src/panels/axesPanel.tsx
+++ src/panels/axesPanel.tsx
@@ -28,6 +28,9 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
   useEffect(() => {
     resize.current = new ResizeObserver(() => {
       viewer.gizmos.axes.resize(gizmoDiv.current.clientWidth)
+      // Remove default styling of the axes
+      viewer.gizmos.axes.canvas.style.top = '0px'
+      viewer.gizmos.axes.canvas.style.right = '0px'
     })
     resize.current.observe(gizmoDiv.current)
 
@@ -38,12 +41,9 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
     if (viewer.gizmos.axes.canvas) {
       gizmoDiv.current.appendChild(viewer.gizmos.axes.canvas)
       viewer.gizmos.axes.canvas.classList.add(
-        'vc-block',
-        '!vc-static',
-        'vc-order-1',
-        'vc-mx-auto',
-        'vc-mb-0',
-        'vc-mt-auto'
+        'vc-absolute',
+        'vc-inset-0',
+        'vc-order-1'
       )
     }
 
@@ -65,19 +65,17 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
   }
 
   const btnStyle =
-    'vim-home-btn vc-flex vc-items-center vc-justify-center vc-text-gray-medium vc-transition-all hover:vc-text-primary-royal'
-  const btnFlex = { width: 'min(2vw, 1.5rem)', height: 'min(2w, 1.5rem)' }
+    'vim-axes-button vc-flex vc-items-center vc-justify-center vc-text-gray-medium vc-transition-all hover:vc-text-primary-royal'
 
   const btnIsolation = (
     <button
-      style={btnFlex}
       data-tip={
         props.settings.value.isolation.useIsolationMaterial
           ? 'Disable Ghosting'
           : 'Enable Ghosting'
       }
       onClick={onIsolationBtn}
-      className={'vim-isolation-btn' + btnStyle}
+      className={'vim-isolation-btn ' + btnStyle}
       type="button"
     >
       {props.settings.value.isolation.useIsolationMaterial
@@ -92,10 +90,9 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
 
   const btnHome = (
     <button
-      style={btnFlex}
       data-tip="Reset Camera"
       onClick={onHomeBtn}
-      className={'vim-home-btn' + btnStyle}
+      className={'vim-home-btn ' + btnStyle}
       type="button"
     >
       <Icons.home height={20} width={20} fill="currentColor" />{' '}
@@ -103,10 +100,9 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
   )
   const btnOrtho = (
     <button
-      style={btnFlex}
       data-tip={ortho ? 'Orthographic' : 'Perspective'}
       onClick={() => (props.viewer.camera.orthographic = !ortho)}
-      className={'vim-camera-btn' + btnStyle}
+      className={'vim-camera-btn ' + btnStyle}
       type="button"
     >
       {ortho
@@ -134,7 +130,7 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
       )
     }
     return (
-      <div className="vim-axes-panel-buttons vc-absolute vc-inset-0 vc-pointer-events-auto vc-order-2 vc-flex vc-justify-evenly vc-bg-white">
+      <div className="vim-axes-panel-buttons vc-absolute vc-inset-0 vc-pointer-events-auto vc-order-2 vc-flex vc-items-center vc-justify-evenly vc-bg-white">
         {createButton(
           btnOrtho,
           props.settings.value.capacity.useOrthographicCamera &&
@@ -150,19 +146,13 @@ function AxesPanel (props: { viewer: VIM.Viewer, camera: ComponentCamera, settin
   }
 
   return (
-    <div style={{
-      borderRadius: 'min(1vw, 1rem)',
-      top: 'min(2vw, 1.5rem)',
-      right: 'min(2vw, 1.5rem)',
-      width: 'min(11vw, 112px)',
-      height: 'min(14vw, 144px)'
-    }}
+    <div
       className={
         'vim-axes-panel vc-absolute vc-overflow-hidden vc-z-20 vc-flex vc-flex-col vc-border vc-border-white vc-opacity-50 vc-shadow-lg vc-saturate-0 vc-transition-all hover:vc-opacity-100 hover:vc-saturate-100' +
         hidden
       }
     >
-      <div ref={gizmoDiv} className='vim-axes-panel-gizmo vc-absolute vc-bottom-[20%] vc-top-2 vc-right-2 vc-left-2'/>
+      <div ref={gizmoDiv} className='vim-axes-panel-gizmo vc-absolute'/>
       <div className='vim-axes-panel-bar vc-absolute vc-top-[80%] vc-bottom-0 vc-right-0 vc-left-0'>
         {createBar()}
       </div>
