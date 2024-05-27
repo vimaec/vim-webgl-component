b6224e3ff40fecaa90f70245621622117f2987cc
vim-sroberge <simon.roberge@vimaec.com>
2024-05-27 10:50:41 -0400

    fixed logo element taking all screen

--- src/panels/logo.tsx
+++ src/panels/logo.tsx
@@ -6,9 +6,9 @@ import React from 'react'
 import logo from '../assets/logo.png'
 
 export const LogoMemo = React.memo(() => (
-  <div className={'vim-logo vc-absolute vc-top-4 vc-left-4'}>
+  <div style={{ width: 'min(128px, 20%' }} className={'vim-logo vc-absolute vc-top-4 vc-left-4 vc-flex'}>
     <a href="https://vimaec.com">
-      <img style={{ width: 'min(128px, 20%' }} className="vim-logo-img" src={logo}></img>
+      <img className="vc-absolute vim-logo-img" src={logo}></img>
     </a>
   </div>
 ))
